4bf1b397ef93fe61182c98d58dbfeb47945b2144
Fede180600 <fede180600@libero.it>
2022-04-22 17:14:13 +0200

    generate numbers

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 //Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+//Genero i 100 numeri
+// creo la array
+const numbers = [];
+
+// imposto il ciclo per generare 100 numeri
+for (let i = 1; i < 100; i++) {
+    let number = Math.floor(Math.random() * 100) + 1;
+    console.log(number);
+    if (number < number - 1 && number > number + 1) {
+        number = Math.floor(Math.random() * 100) + 1;
+    } else {
+        numbers.push(number);
+    }
+}
+
+console.log(numbers);

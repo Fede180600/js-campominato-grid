a2a2bb86fd1f8a644c45d17e1812df39577a5675
Fede180600 <fede180600@libero.it>
2022-04-25 15:26:15 +0200

    add eventListener at button

--- js/script.js
+++ js/script.js
@@ -2,23 +2,24 @@
 
 // Prelevo il botton play
 const myButton = document.getElementById('play');
-console.log(play);
-
-//Genero i 100 numeri
-const gridContainer = document.querySelector('.grid-container');
-
-for (let i = 0; i < 100; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('grid-item');
-    cell.innerHTML = i + 1;
-    gridContainer.append(cell);
-
-    // Aggiungere la classe hover al click
-    cell.addEventListener("click", function() {
-        this.classList.add('active');
-    });
-
-}
+myButton.addEventListener("click", function() {
+    //Genero i 100 numeri
+    const gridContainer = document.querySelector('.grid-container');
+    
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-item');
+        cell.innerHTML = i + 1;
+        gridContainer.append(cell);
+    
+        // Aggiungere la classe hover al click
+        cell.addEventListener("click", function() {
+            this.classList.add('active');
+        });
+    
+    }
+    // gridContainer.classList.add("d-block");
+});
 
 
 
